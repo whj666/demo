2d43bccbfdf12c324bf9a8d0d6d4262fed1db9d5
wanghongjin <1010349053@qq.com>
2018-06-07 22:07:36 +0800

    1

--- src/container/top/index.jsx
+++ src/container/top/index.jsx
@@ -10,12 +10,26 @@ import * as actionAll from 'actionAll';
 class Top extends React.Component{
     constructor(props){
         super();
+
+        this.state = {
+            flag: false
+        }
     }
 
+    //菜单收起展开
     collapsed(){
         this.props.actionAll.collapsed({
             collapsed: !this.props.stateAll.common.collapsed
         })
+
+        this.setState({
+            flag: !this.state.flag
+        })
+    }
+
+    //登出
+    signOut(){
+        location.hash = "#/login";
     }
 
     render(){
@@ -23,13 +37,13 @@ class Top extends React.Component{
         	<div className="topBox">
                 <div className="topBox-nav cb">
                     <div className="icon fl">
-                        <Icon onClick={this.collapsed.bind(this)} type="menu-fold" />
+                        <Icon onClick={this.collapsed.bind(this)} type={this.state.flag ? "menu-unfold" : "menu-fold"} />
                     </div>
 
                     <div className="fl f16 systemName">CMS SYSTEM</div>
 
                     <div className="fr">
-                        <Icon type="logout" />
+                        <Icon onClick={this.signOut.bind(this)} type="logout" />
                     </div>
 
                     <div className="fr userName f16">admin</div>
