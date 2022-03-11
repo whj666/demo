0fe0b13dda5e6ee21b317512e0a9464528e87d9c
zwb_wanghongjin <hongjin.wang@zatech.com>
2022-03-11 13:48:39 +0800

    加预览的路由

---
+++ src/container/View/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class View extends Component {
+  render() {
+    const { match } = this.props;
+
+    return <div>hello world</div>;
+  }
+}
+
+export default View;
--- src/router/routeMap.jsx
+++ src/router/routeMap.jsx
@@ -2,27 +2,34 @@ import React from "react";
 import { Route, HashRouter, Switch, Redirect } from "react-router-dom";
 import "../public/public.less";
 import Login from "../container/login";
+import View from "../container/View";
 import App from "../container";
 
 class RouterMap extends React.Component {
-
   //权限验证
   check = () => {
-    return sessionStorage.login ? <App /> : <Redirect to={{
-      pathname: '/login'
-    }}/>
-  }
+    return sessionStorage.login ? (
+      <App />
+    ) : (
+      <Redirect
+        to={{
+          pathname: "/login",
+        }}
+      />
+    );
+  };
 
   render() {
     return (
       <HashRouter>
-	      <Switch>
-	      	<Route  path="/login" component={Login} />
-	        <Route path="/" render={this.check} />
-	      </Switch>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/view/:type/:id" component={View} />
+          <Route path="/" render={this.check} />
+        </Switch>
       </HashRouter>
-    )
+    );
   }
 }
 
-export default RouterMap
+export default RouterMap;
