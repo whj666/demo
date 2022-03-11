3ca117ffbebe750da6c7f7bcfb71db423dc73c73
zwb_wanghongjin <hongjin.wang@zatech.com>
2022-03-11 10:18:00 +0800

    格式化

--- src/container/body/funModule/curd/searchBar.jsx
+++ src/container/body/funModule/curd/searchBar.jsx
@@ -1,82 +1,85 @@
 import React from "react";
-import {Form, Button, Input, InputNumber} from 'antd';
-import {postApi} from "api";
-import {urls} from "urls";
-import "./style"
+import { Form, Button, Input, InputNumber } from "antd";
+import { postApi } from "api";
+import { urls } from "urls";
+import "./style";
 
 const FormItem = Form.Item;
 
-class SearchBar extends React.Component{
-    constructor(props){
-        super(props);
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
+    this.state = {};
+  }
 
-        }
-    }
+  componentDidMount() {}
 
-    componentDidMount(){
-    
-    }
-
-    //查询
-    handleSubmit = () => {
-        this.props.form.validateFieldsAndScroll((err, values) => {
-            if(!err) {
-                this.props.getTableData(Object.assign({}, {userName:localStorage.userName}, values));
-            }
-        });
-    }
-
-    //重置
-    reset(){
-        this.props.form.resetFields();
-    }
-
-    //新建
-    new(){
-        this.props.this.visibleHandle();
-    }
-  
-    render(){
-        return(
-            <div className="searchBar oh">
-                <Form className="fl" layout="inline">
-                    <FormItem label='姓名' >
-                        {this.props.form.getFieldDecorator('name', {
-                            initialValue: ""
-                        })(
-                            <Input placeholder="请输入" />
-                        )}
-                    </FormItem>
-
-                    <FormItem label='最小年龄' >
-                        {this.props.form.getFieldDecorator('minAge', {
-                            initialValue: ""
-                        })(
-                            <InputNumber placeholder="请输入" />
-                        )}
-                    </FormItem>
-
-                    <FormItem label='最大年龄' >
-                        {this.props.form.getFieldDecorator('maxAge', {
-                            initialValue: ""
-                        })(
-                            <InputNumber placeholder="请输入" />
-                        )}
-                    </FormItem>
-
-                    <FormItem>
-                        <Button type="primary" icon="search" onClick={this.handleSubmit}>查询</Button>
-                        <Button className="ml10" onClick={this.reset.bind(this)}>重置</Button>
-                    </FormItem>
-                </Form>
-
-                <Button icon="profile" className="fr" type="primary" onClick={this.new.bind(this)}>新建</Button>
-            </div>
+  //查询
+  handleSubmit = () => {
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        this.props.getTableData(
+          Object.assign({}, { userName: localStorage.userName }, values)
         );
-    }
+      }
+    });
+  };
+
+  //重置
+  reset() {
+    this.props.form.resetFields();
+  }
+
+  //新建
+  new() {
+    this.props.this.visibleHandle();
+  }
+
+  render() {
+    return (
+      <div className="searchBar oh">
+        <Form className="fl" layout="inline">
+          <FormItem label="姓名">
+            {this.props.form.getFieldDecorator("name", {
+              initialValue: "",
+            })(<Input placeholder="请输入" />)}
+          </FormItem>
+
+          <FormItem label="最小年龄">
+            {this.props.form.getFieldDecorator("minAge", {
+              initialValue: "",
+            })(<InputNumber placeholder="请输入" />)}
+          </FormItem>
+
+          <FormItem label="最大年龄">
+            {this.props.form.getFieldDecorator("maxAge", {
+              initialValue: "",
+            })(<InputNumber placeholder="请输入" />)}
+          </FormItem>
+
+          <FormItem>
+            <Button type="primary" icon="search" onClick={this.handleSubmit}>
+              查询
+            </Button>
+            <Button className="ml10" onClick={this.reset.bind(this)}>
+              重置
+            </Button>
+          </FormItem>
+        </Form>
+
+        <Button
+          icon="profile"
+          className="fr"
+          type="primary"
+          onClick={this.new.bind(this)}
+        >
+          新建
+        </Button>
+      </div>
+    );
+  }
 }
 
 const _SearchBar = Form.create()(SearchBar);
-export default _SearchBar
+export default _SearchBar;
